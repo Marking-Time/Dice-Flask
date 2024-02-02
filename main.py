a44ebb6261bb1bcd27aa62e4ddfeb3475430efa3
Marking-Time <153015560+Marking-Time@users.noreply.github.com>
2024-02-02 19:00:42 +0000

    Add: Changes to main

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 from flask import Flask, render_template
 
-app = Flask(__name__,
-           template_folder='templates', 
-           static_folder='static'
-           )
+app = Flask(__name__, template_folder='templates' )
 
 
-# @app.route('/')
-# def base_page():
-  # return render_template('base.html')
+@app.route('/')
+def base_page():
+  return render_template('base.html')
 
 # def index():
 #     return '<h1>Jello from Flask!</h1>'
 
-@app.route('/templates')
-def index():
-      return render_template('index.html',)
+# @app.route('/templates')
+# def index():
+#   return render_template('templates/base.html')
 
 
 app.run(host='0.0.0.0', port=81)
