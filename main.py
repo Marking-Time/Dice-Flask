e0ca3e504514b7d7c2240b93162594716ecc48da
Marking-Time <153015560+Marking-Time@users.noreply.github.com>
2024-02-02 18:15:56 +0000

    Add: Changes to flask

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__,
+           template_folder='templates', 
+           static_folder='static'
+           )
 
 
 @app.route('/')
-def index():
-    return '<h1>Jello from Flask!</h1>'
+def base_page():
+  return render_template('base.html')
+
+# def index():
+#     return '<h1>Jello from Flask!</h1>'
 
 
 app.run(host='0.0.0.0', port=81)
