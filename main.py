63403c1de461e8ed4b41f485971765110eb97598
Marking-Time <153015560+Marking-Time@users.noreply.github.com>
2024-02-02 18:41:09 +0000

    Add: Changes to main to make flask run

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__,
            template_folder='templates', 
@@ -6,12 +6,16 @@ app = Flask(__name__,
            )
 
 
-@app.route('/')
-def base_page():
-  return render_template('base.html')
+# @app.route('/')
+# def base_page():
+  # return render_template('base.html')
 
 # def index():
 #     return '<h1>Jello from Flask!</h1>'
+
+@app.route('/templates')
+def index():
+      return render_template('index.html',)
 
 
 app.run(host='0.0.0.0', port=81)
